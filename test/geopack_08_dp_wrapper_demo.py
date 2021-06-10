54a26f22250d87bddef9c4c0d7aea9258bf4cf2f
Bob Weigel <rweigel@gmu.edu>
2021-06-10 16:59:57 -0400

    Comment on timeformat

--- test/geopack_08_dp_wrapper_demo.py
+++ test/geopack_08_dp_wrapper_demo.py
@@ -15,10 +15,16 @@ def xprint(msg):
 
 time = (1997,1,1)
 input = [1., 0., 0.]
+# Time format for hxform is
+# year, month, day, [hours, [minutes, [seconds]]]
+# only year, month, day are required.
 output = hx.GSMtoSM(input, time, lib='geopack_08_dp')
 xprint(input)
 xprint(output)
 
+# Time format for geopack_08_dp is
+# year, doy, hour, minute, second
+# all values are required.
 dtime = np.array((1997,1,1,0,0), dtype=np.int32)
 input = [1., 0., 0.]
 output = np.column_stack(geopack_08_dp.transform(input[0], input[1], input[2], "GSMtoSM", dtime))
