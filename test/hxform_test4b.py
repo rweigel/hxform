bb2262da4dce797daac076fb4b97bdf6c462db57
Gary Quaresima <garyquaresima@gmail.com>
2021-08-06 17:46:45 -0400

    update test4b

--- test/hxform_test4b.py
+++ test/hxform_test4b.py
@@ -10,10 +10,10 @@ t = [2009,2,2,11,1,1]
 ##################################
 
 vt = trans(v,t)
-print(type(vt))
-print(vt)
-#assert(type(vt) == list)
-#assert(type(vt[0]) == float)
+#print(type(vt))
+#print(vt)
+assert(type(vt) == list)
+assert(type(vt[0]) == np.double)
 assert(len(vt) == 3)
 
 vt = trans(np.array(v),np.array(t))
@@ -47,8 +47,8 @@ assert(len(vt) == 2)
 assert(len(vt[0]) == 3)
 assert(len(vt[1]) == 3)
 for i in range(3):
-    print(type(vt[0][i]))
-    print(type(vt[1][i]))
+    assert(type(vt[0][i]) == np.double)
+    assert(type(vt[1][i]) == np.double)
 
 vt = trans(np.array(v),np.array(t))
 assert(type(vt) == np.ndarray)
@@ -68,10 +68,7 @@ assert(len(vt) == 1)
 assert(type(vt[0]) == np.ndarray)
 assert(type(vt[0][0]) == np.double)
 
-
-
-a = trans([v],[t])
-
+#############################################
 
 v2n   = np.array([[1,2,3],[4,5,6]])
 v1n_0 = np.array([[1,2,3]])
@@ -98,13 +95,19 @@ assert( trans(v1n_0,tn_0).shape == (1,3) )
 assert( trans(vn_0 ,tn_0).shape == ( 3,) )
 
 assert(np.all( trans(v2n  ,t2n)==np.array([trans(vn_0,tn_0),trans(vn_1,tn_1)]) ))
+assert(np.all( trans(vn_0,tn_0) == trans(v1n_0,t1n_0).ravel() ))
+assert(np.all( trans(vn_0,tn_0) == trans(v1n_0, tn_0).ravel() ))
+assert(np.all( trans(vn_0,tn_0) == trans( vn_0,t1n_0).ravel() ))
+assert(np.all( trans(vn_1,tn_1) == trans(v1n_1,t1n_1).ravel() ))
+assert(np.all( trans(vn_1,tn_1) == trans(v1n_1, tn_1).ravel() ))
+assert(np.all( trans(vn_1,tn_1) == trans( vn_1,t1n_1).ravel() ))
 
-print(trans(v2n  ,t2n))
-print(trans(vn_0,tn_0))
-print(trans(vn_1,tn_1))
-print(trans(v1n_0,tn_0))
-print(trans(v1n_1,tn_1))
-print(trans(vn_0,t1n_0))
-print(trans(vn_1,t1n_1))
-print(trans(v1n_0,t1n_0))
-print(trans(v1n_1,t1n_1))
+#print(trans(v2n  ,t2n))
+#print(trans(vn_0,tn_0))
+#print(trans(vn_1,tn_1))
+#print(trans(v1n_0,tn_0))
+#print(trans(v1n_1,tn_1))
+#print(trans(vn_0,t1n_0))
+#print(trans(vn_1,t1n_1))
+#print(trans(v1n_0,t1n_0))
+#print(trans(v1n_1,t1n_1))
