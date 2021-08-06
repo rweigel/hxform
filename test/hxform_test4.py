745640c73e97ba27791d1b5a634e6b28f90eb150
Bob Weigel <rweigel@gmu.edu>
2021-08-06 16:23:52 -0400

    new tests

--- test/hxform_test4.py
+++ test/hxform_test4.py
@@ -4,6 +4,73 @@ from hxform import hxform as hx
 def trans(v,t):
     return hx.transform(v, t, 'GSM','MAG', lib='geopack_08_dp')
 
+v = [1,2,3]
+t = [2009,2,2,11,1,1]
+
+##################################
+
+vt = trans(v,t)
+#assert(type(vt) == list)
+#assert(type(vt[0]) == float)
+#assert(len(vt) == 3)
+
+vt = trans(np.array(v),np.array(t))
+#assert(type(vt) == np.ndarray)
+#assert(type(vt[0]) == np.double)
+#assert(vt.shape == (3,))
+
+##################################
+
+##################################
+vt = trans([v],[t])
+print(vt)
+print(type(vt))
+#assert(type(vt) == list)
+#assert(type(vt[0]) == list)
+#assert(len(vt) == 1)
+#assert(len(vt[0]) == 3)
+
+vt = trans(np.array([v]),np.array([t]))
+assert(type(vt) == np.ndarray)
+assert(vt.shape == (1,3))
+assert(type(vt[0,0]) == np.double)
+
+##################################
+
+vt = trans([v,v],[t,t])
+#assert(type(vt) == list)
+#assert(type(vt[0]) == list)
+#assert(type(vt[1]) == list)
+#assert(len(vt) == 2)
+#assert(len(vt[0]) == 3)
+#assert(len(vt[1]) == 3)
+#for i in range(3):
+#    assert(type(vt[0][i]) == float)
+#    assert(type(vt[1][i]) == float)
+
+vt = trans(np.array(v),np.array(t))
+assert(type(vt) == np.ndarray)
+assert(vt.size == 3)
+for i in range(3):
+    assert(type(vt[i]) == np.double)
+
+
+vt = trans(np.array([v]),np.array([t]))
+assert(type(vt) == np.ndarray)
+assert(vt.shape == (1,3))
+
+
+vt = trans([np.array(v)],[np.array(t)])
+assert(type(vt) == list)
+assert(len(vt) == 1)
+assert(type(vt[0]) == np.ndarray)
+assert(type(vt[0][0]) == np.double)
+
+
+
+a = trans([v],[t])
+
+
 v2n   = np.array([[1,2,3],[4,5,6]])
 v1n_0 = np.array([[1,2,3]])
 v1n_1 = np.array([[4,5,6]])
