427e0b958c609c56680e2c9a5f9bcd45f9fe37c3
Bob Weigel <rweigel@gmu.edu>
2024-03-29 16:46:27 -0400

    Geopack wrapper fix

--- demo/geopack_08_dp_wrapper_demo.py
+++ demo/geopack_08_dp_wrapper_demo.py
@@ -9,22 +9,6 @@ from hxform import xprint # print to console and geopack_08_dp_demo.log
 # Time format for geopack_08_dp is
 # year, doy, hour, minute, second
 # all values are required.
-dtime = np.array([[1997,1,1,0,0]], dtype=np.int32)
+dtime = np.array([[1997, 1, 1, 0, 0]], dtype=np.int32)
 input = np.array([[1., 0., 0.]], dtype=np.float64)
-output = np.column_stack(geopack_08_dp.transform(input, "GSMtoGSE", dtime,1))
-
-xprint(input)
-xprint(output)
-
-if False:
-    input = [[1., 0., 0.],[1., 0., 0.]]
-    output = hx.GSMtoSM(input, time, lib='geopack_08_dp')
-    xprint(input)
-    xprint(output)
-
-    dtime = np.array((1997,1,1,0,0), dtype=np.int32)
-    input = [[1., 0., 0.],[1., 0., 0.],[1., 0., 0.]]
-    output = np.column_stack(geopack_08_dp.transform(input[0], input[1], input[2], "GSMtoGSE", dtime))
-
-    xprint(input)
-    xprint(output)
+output = geopack_08_dp.transform(input, "GSMtoGSE", dtime, 1)
