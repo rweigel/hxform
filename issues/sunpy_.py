8c1e53352ecda5856f076cf82fb153a4c281b848
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-05-14 16:00:25 -0400

    GSM

--- issues/sunpy_.py
+++ issues/sunpy_.py
@@ -13,4 +13,4 @@ data_jpl = get_horizons_coord(sc_id, "2021-11-25T00:00:00").geocentricsolareclip
 print(data_jpl)
 print(data_jpl.value/6378.16)
 
-curl 'https://ssd.jpl.nasa.gov/api/horizons_file.api?!$$SOF&EPHEM_TYPE=VECTORS&OUT_UNITS=AU-D&CENTER=500@10&VEC_TABLE=2&CSV_FORMAT=YES&COMMAND='-78'&TLIST=2459543.500800728&TLIST_TYPE=JD'
+#curl 'https://ssd.jpl.nasa.gov/api/horizons_file.api?!$$SOF&EPHEM_TYPE=VECTORS&OUT_UNITS=AU-D&CENTER=500@10&VEC_TABLE=2&CSV_FORMAT=YES&COMMAND='-78'&TLIST=2459543.500800728&TLIST_TYPE=JD'
