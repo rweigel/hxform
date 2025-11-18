a5b1a258e976647ed0f3aed09eb87c7948effa08
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-11-18 17:53:07 -0500

    clean-up

--- demo/comparisons/compare.py
+++ demo/comparisons/compare.py
@@ -1,7 +1,7 @@
 import numpy as np
 import hxform
 
-d = 6.0
+d = 1.0
 v = d*np.array([1., 1., 1.])/np.sqrt(3)
 t = [2013, 12, 30, 0, 0, 0]
 kwargs = {
@@ -9,14 +9,9 @@ kwargs = {
   "frame_out": "GSM",
   "rep_in": "car",
   "rep_out": "car",
-#  "libs_exclude": ['sscweb']
+  "libs_exclude": ['sscweb', 'pyspedas']
 }
 
 results = hxform.compare(v, t, **kwargs)
 with open(f'compare-{kwargs["frame_in"]}2{kwargs["frame_out"]}.txt', 'w') as f:
   f.write(results["log"])
-
-#print(results)
-results['dtheta'] = 0.087
-dd = d*results['dtheta']*(np.pi/180.0)*6378.16
-print(f"If x, y, z are in R_E, then max-min angle corresponds to {dd:.2f} km")
