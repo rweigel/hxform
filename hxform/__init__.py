95ff2df5eb91b406ea22e03c58f4a5a634f689d1
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-11-20 11:06:47 -0500

    mv xprint to utilrsw

--- hxform/__init__.py
+++ hxform/__init__.py
@@ -1,11 +1,13 @@
-# SpacePy
 import warnings
+# SpacePy
 warnings.filterwarnings("ignore", message="Leapseconds.*")
 
 __version__ = '0.1.0'
-# TODO: Don't import everything. Only import what is needed. Do this using __all__.
+
+# TODO: Don't import everything. Only import what is needed.
 from hxform.hxform import *
 from hxform.timelib import *
 from hxform.info import *
-from hxform.xprint import xprint
 from hxform.compare import compare
+
+from utilrsw.xprint import xprint
