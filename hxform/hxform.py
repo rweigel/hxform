7249289fd5dead9ef669ff330a123555f688382c
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-11-27 10:55:41 -0500

    c2s, s2c testing

--- hxform/hxform.py
+++ hxform/hxform.py
@@ -525,47 +525,54 @@ def matrix(t, frame_in, frame_out, lib='geopack_08_dp'):
 def components2matrix(*args):
   """Convert input to a 2D numpy array with three columns.
 
-  For three inputs, components2matrix(x, y, z), each input contains a value(s)
-  for one component (x, y, z).  The output matrix has columns of x, y, and z.
-  List and tuple inputs must have same length.
+  For three arguments, components2matrix(x, y, z), each argument contains a value
+  (or values) for one component (x, y, z). The output matrix has columns
+  of x, y, and z. List and tuple inputs must have same length. Returned matrix
+  is numpy.column_stack([x, y, z]).
     1. components2matrix(number, number, number)
     2. components2matrix(list, list, list)
     3. components2matrix(tuple, tuple, tuple)
+    4. components2matrix(ndarray, ndarray, ndarray)
 
-  For one input, components2matrix(v), the input contains all three components.    :
-    Output matrix has columns of x, y, and z and 1 row.
-      4. components2matrix([number, number, number])
-      5. components2matrix((number, number, number))
-    Output matrix has columns of x, y, and z and N rows
-      6. components2matrix([[number, number, number], ...]) (N lists of 3 numbers)
-      7. components2matrix(((number, number, number), ...)) (N tuples of 3 numbers)
+  For one argument, components2matrix(v), the argument contains all three components.
+    Output matrix has columns of x, y, and z and 1 row. Numbers must be of same type.
+      5. components2matrix([number, number, number])
+      6. components2matrix((number, number, number))
+    Output matrix has columns of x, y, and z and N rows. Numbers are coerced to same type.
+      7. components2matrix([[number, number, number], ...]) (N lists of 3 numbers)
+      8. components2matrix(((number, number, number), ...)) (N tuples of 3 numbers)
     Output matrix has columns of x, y, and z and 1 row
-      8. components2matrix(size = (3,) ndarray)
-      9. components2matrix(size = (1, 3) ndarray)
+      9. components2matrix(size = (3,) ndarray)
+      10. components2matrix(size = (1, 3) ndarray)
     Output is same as input
-      10. components2matrix((N, 3) ndarray)
+      11. components2matrix((N, 3) ndarray)
 
   """
   import numpy
-
   if len(args) != 1 and len(args) != 3:
     raise ValueError(f'Number of arguments must be 1 or 3, got {len(args)}.')
+
 
   number_types = (int, float, numpy.integer, numpy.floating)
 
   if len(args) == 3:
     # _components(x, y, z)
-
+    coda = 'when passing three arguments.'
     for arg in args:
-      # x, y, and z must have same type
-      if not isinstance(arg, type(args[0])):
-        ve = 'All inputs must be of the same type when passing three arguments.'
-        raise ValueError(ve)
       if not (isinstance(arg, number_types) or
               isinstance(arg, (list, tuple)) or
               isinstance(arg, numpy.ndarray)):
-        ve = 'All inputs must be number, list, tuple, or numpy.ndarray when passing three arguments.'
+        ve = f'All inputs must be number, list, tuple, or numpy.ndarray {coda}'
         raise ValueError(ve)
+
+      if isinstance(args[0], number_types):
+        if not isinstance(arg, number_types):
+          ve = f'If the first input is a number, all inputs must numbers {coda}'
+          raise ValueError(ve)
+      else:
+        if not isinstance(arg, type(args[0])):
+          ve = f'If the first input is not numeric, all inputs have same type {coda}'
+          raise ValueError(ve)
 
     if isinstance(args[0], number_types):
       # _components(number, number, number)
@@ -573,68 +580,124 @@ def components2matrix(*args):
       return numpy.column_stack([args[0], args[1], args[2]])
 
     if isinstance(args[0], (list, tuple)):
-      # _components(list, list, list) or _components(tuple, tuple, tuple)
+      # _components(list, list, list)
+      # or
+      # _components(tuple, tuple, tuple)
       for arg in args:
         if not isinstance(arg, number_types):
           if len(arg) != len(args[0]):
-            ve = 'All arguments must have same length when passing three arguments.'
+            ve = f'All arguments must have same length {coda}'
             raise ValueError(ve)
       # Return 2D array with columns of component
       return numpy.column_stack([args[0], args[1], args[2]])
 
     if isinstance(args[0], numpy.ndarray):
       # _components(ndarray, ndarray, ndarray)
+      if args[0].ndim == 0:
+        return numpy.array([[args[0], args[1], args[2]]])
+
       for arg in args:
         if len(arg.shape) != 1:
-          ve = 'All numpy.ndarrays must be 1D when passing three arguments.'
+          ve = f'All numpy.ndarrays must be 1D {coda}'
           raise ValueError(ve)
         if arg.shape[0] != args[0].shape[0]:
-          ve = 'All numpy.ndarrays must have same length when passing three arguments.'
+          ve = f'All numpy.ndarrays must have same shape[0] {coda}'
           raise ValueError(ve)
+
       # Return 2D array with columns of component
       return numpy.column_stack([args[0], args[1], args[2]])
 
-  if len(args) == 1:
 
+  if len(args) == 1:
+    coda = 'when passing one argument.'
     if isinstance(args, (list, tuple)):
       # _components(list) or _components(tuple)
       if isinstance(args[0], (list, tuple)):
-        # _components([list of 3 values, ...]) or _components((tuple of 3 values, ...))
+        # _components([list of 3 values, ...])
+        # or
+        # _components((tuple of 3 values, ...))
         for arg in args[0]:
-          if not isinstance(arg, type(args[0])):
-            ve = 'All elements of input list/tuple must be of same type when passing one argument.'
+          if isinstance(args[0][0], number_types):
+            if not isinstance(arg, number_types):
+              ve = f'If the first input is a number, all inputs must numbers {coda}'
+              raise ValueError(ve)
+          else:
+            if not isinstance(arg, type(args[0][0])):
+              ve = f'If the first input is not numeric, all inputs must have same type {coda}'
+              raise ValueError(ve)
+
           if isinstance(arg, (list, tuple)):
             if len(arg) != 3:
-              ve = 'All elements of input list/tuple must have three elements when passing one argument.'
+              ve = f'All list/tuples must have three elements {coda}'
               raise ValueError(ve)
 
         if isinstance(args[0][0], number_types):
-          # _components([number, number, number]) or _components((number, number, number))
-          return numpy.array([[args[0][0], args[0][1], args[0][2]]])
+          if len(args[0]) != 3:
+            ve = f'Input list/tuple must have three elements {coda}'
+            raise ValueError(ve)
+          # _components([number, number, number])
+          # or
+          # _components((number, number, number))
+          return numpy.array(args[0]).reshape((1, 3))
         else:
-          # _components([[number, number, number], ...]) or _components(((number, number, number), ...))
+          # _components([[number, number, number], ...])
+          # or
+          # _components(((number, number, number), ...))
+          # Does not test that each number is of same type
           return numpy.array(args[0])
 
       if isinstance(args[0], numpy.ndarray):
         # _components(ndarray)
 
         if len(args[0].shape) != 1 and len(args[0].shape) != 2:
-          ve = 'Input numpy.ndarray must be 1D or 2D when passing one argument.'
+          ve = f'Input numpy.ndarray must be 1D or 2D {coda}'
           raise ValueError(ve)
 
         if len(args[0].shape) == 1:
           # _components(1D ndarray)
           if args[0].shape[0] != 3:
-            ve = 'Input 1-D numpy.ndarray must have three elements when passing one argument.'
+            ve = f'Input 1-D numpy.ndarray must have three elements {coda}'
             raise ValueError(ve)
           return numpy.array([args[0]])
 
         if len(args[0].shape) == 2:
           # _components((n, 3) ndarray)
           if args[0].shape[1] != 3:
-            ve = 'Input numpy.ndarray must have three columns when passing one argument.'
+            ve = f'Input numpy.ndarray must have three columns {coda}'
             raise ValueError(ve)
           return args[0]
+
+
+def matrix2components(*args):
+  """Given args passed to components2matrix, return values matching input type.
+  """
+  import numpy as np
+  out = []
+  if len(args) != 4 and len(args) != 1:
+    raise ValueError(f'Number of arguments must be 1 or 4, got {len(args)}.')
+
+  if len(args) == 4:
+    # Input was components2matrix(x, y, z, mat)
+    mat = args[3]
+    for i in range(3):
+      comp = mat[:, i]
+      if isinstance(args[i], np.ndarray):
+        out.append(np.array(comp).reshape(args[i].shape))
+      elif isinstance(args[i], (list, tuple)):
+        out.append(type(args[i])(comp.tolist()))
+      else:
+        out.append(comp[0])
+    return tuple(out)
+
+  if len(args) == 1:
+    # Input was components2matrix(v, mat)
+    mat = args[0]
+    if isinstance(args[0], np.ndarray):
+      return np.array(mat).reshape(args[0].shape)
+    elif isinstance(args[0], list):
+      return [list(row) for row in mat.tolist()]
+    elif isinstance(args[0], tuple):
+      return tuple(tuple(row) for row in mat.tolist())
 
 
 def car2sph(*args):
@@ -654,7 +717,7 @@ def car2sph(*args):
   lat = 90.0 - (180.0/np.pi)*np.arccos(z/r)
   lon = (180.0/np.pi)*np.arctan2(y, x)
 
-  return np.column_stack([r, lat, lon])
+  return matrix2components(*args, np.column_stack([r, lat, lon]))
 
 
 def sph2car(*args):
@@ -676,10 +739,7 @@ def sph2car(*args):
   y = r*np.sin((np.pi/180.0)*lon)*np.cos((np.pi/180.0)*lat)
   z = r*np.sin((np.pi/180.0)*lat)
 
-  if len(args) == 1:
-    return np.column_stack([x, y, z])
-
-  return x, y, z
+  return matrix2components(*args, np.column_stack([x, y, z]))
 
 
 def get_spherical_vector_components(v_cart, x_cart):
