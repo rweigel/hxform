5783fed0b8a1be40e439ec2b35fd57b995e0b327
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-01-12 13:48:56 -0500

    subsolar updates

--- hxform/car2sph.py
+++ hxform/car2sph.py
@@ -4,8 +4,8 @@ def car2sph(*args):
     r, lat, lon = car2sph(x, y, z) where x, y, z are 1D arrays, lists, or
     tuples of the same length.
 
-    rtp = car2sph(np.array(xyz)) where xyz is a 2D array-like object with
-    shape (N, 3). xyz may be a list/tuple of lists/tuples or 1D numpy arrays.
+    rtp = car2sph(xyz) where xyz is a 2D array-like object with shape (N, 3).
+    xyz may be a list/tuple of lists/tuples or 1D numpy arrays.
     rtp (r, lat, lon) has the same structure as the input.
   """
 
@@ -24,7 +24,7 @@ def car2sph(*args):
   if not np.all(r > 0):
     raise ValueError('x^2 + y^2 + z^2 must be greater than zero.')
 
-  lat = 90.0 - (180.0/np.pi)*np.arccos(z/r)
+  colat = 90.0 - (180.0/np.pi)*np.arccos(z/r)
   lon = (180.0/np.pi)*np.arctan2(y, x)
 
-  return matrix2components(*args, np.column_stack([r, lat, lon]))
+  return matrix2components(*args, np.column_stack([r, colat, lon]))
