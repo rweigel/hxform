8338204f150c36756d8d0f19ccddf810bd287e3b
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-02-05 14:29:13 -0500

    update demos

--- demo/basic.py
+++ demo/basic.py
@@ -1,15 +1,14 @@
 import hxform
 
+# See basic.log for expected output
 lib = 'spiceypy1'
+
 final = 'GSM'
 initial = 'GSE'
 time1 = '2010-12-30T00:00:00Z'
 time2 = '2010-12-31T01:00:00Z'
 input1 = [1., 1., 1.]
 input2 = [1., 0., 1.]
-
-time1 = hxform.timelib.iso2ints(time1)
-time2 = hxform.timelib.iso2ints(time2)
 
 # Transform one input at one time
 hxform.xprint("")
@@ -38,7 +37,6 @@ output = hxform.transform([input1, input2], time1, initial, final, lib=lib)
 hxform.xprint(f"Output: {output}\n")
 
 # Transform two inputs, each at different times
-time2 = [2010, 12, 31, 0, 0, 0]
 hxform.xprint("")
 hxform.xprint(f"Transform: {initial} => {final}")
 hxform.xprint(f"Time:   {[time1, time2]}")
