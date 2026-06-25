54fe0ce7e6aca673d9e95e3ca751bf5e859941b0
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-06-25 13:33:53 -0400

    gmst

--- demo/gmst.py
+++ demo/gmst.py
@@ -160,6 +160,54 @@ def timestr2deg(ts):
 def gmstR71(iyear, iday, secs):
   """Greenwich Mean Sidereal Time in degrees using Russell 1971 formula."""
 
+  # http://jsoc.stanford.edu/~jsoc/keywords/Chris_Russel/Geophysical%20Coordinate%20Transformations.htm
+  """
+  The following is from
+  http://jsoc.stanford.edu/~jsoc/keywords/Chris_Russel/Geophysical%20Coordinate%20Transformations.htm
+  which is a copy of a page previous posted at on Russell's website.
+
+  Appendix 2. The Calculation of the Position of the Sun
+
+  G.D. Mead (private communication) has written a simple subroutine to calculate
+  the position of the Sun in GEI coordinates. It is accurate for years 1901
+  through 2099, to within 0.006 deg. The input is the year, day of year and
+  seconds of the day in UT. The output is Greenwich Mean Sideral Time in degrees,
+  the ecliptic longitude, apparent right ascension and declination of the Sun
+  in degrees. The listing of this program follows. We note that the cartesian
+  coordinates of the vector from the Earth to the Sun are:
+
+    X = cos(SRASN) cos(SDEC)
+    Y = sin(SRASN) cos(SDEC)
+    Z = sin(SDEC)
+
+    SUBROUTINE SUN(IYR, IDAY, SECS, GST, SLONG, SRASN, SDEC)
+  C PROGRAM TO CALCULATE SIDEREAL TIME AND POSITION OF THE SUN.
+  C GOOD FOR YEARS 1901 THROUGH 2099. ACCURACY 0.006 DEGREE.
+  C INPUT IS IYR, IDAY (INTEGERS), AND SECS, DEFINING UN. TIME.
+  C OUTPUT IS GREENWICH MEAN SIDEREAL TIME (GST) IN DEGREES,
+  C LONGITUDE ALONG ECLIPTIC (SLONG), AND APPARENT RIGHT ASCENSION
+  C AND DECLINATION (SRASN, SDEC) OF THE SUN, ALL IN DEGREES
+    DATA RAD /57.29578/
+    DOUBLE PRECISION DJ, FDAY
+    IF(IYR. LT. 1901. OR. IYR. GT. 2099) RETURN
+    FDAY = SECS/86400
+    DJ = 365* (IYR-1900) + (IYR-1901)/4 + IDAY + FDAY -0.5D0 
+    T = DJ / 36525
+    VL = DMOD (279.696678 + 0.9856473354*DJ, 360.D0)
+    GST = DMOD (279.690983 + 0.9856473354*DJ + 360.*FDAY + 180., 360.D0)
+    G = DMOD (358.475845 + 0.985600267*DJ, 360.D0) / RAD
+    SLONG = VL + (1.91946 -0.004789*T)*SIN(G) + 0.020094*SIN (2.*G)
+    OBLIQ = (23.45229 -0.0130125*T) / RAD
+    SLP = (SLONG -0.005686) / RAD
+    SIND = SIN (OBLIQ)*SIN (SLP)
+    COSD = SQRT(1.-SIND**2)
+    SDEC = RAD * ATAN (SIND/COSD)
+    SRASN = 180. -RAD*ATAN2
+    (COTAN (OBLIQ)*SIND/COSD, -COS (SLP)/COSD)
+    RETURN
+    END
+  """
+
   checkargs(iyear, iday, secs)
   if iyear < 1901 or iyear > 3000:
     raise ValueError(f"year must be in the range [1901, 2099], got {iyear}")
@@ -169,30 +217,70 @@ def gmstR71(iyear, iday, secs):
 
   gst_deg = (279.690983 + 0.9856473354*dj + 360.0*fday + 180.0) % 360.0
 
+  # Geopack-2008 version of this is
+  # DATA RAD/57.295779513D0/
+  # GST=DMOD(279.690983D0 + .9856473354D0*DJ + 360.D0*FDAY + 180.D0, 360.D0)/
+  # * RAD
+  # (the * before RAD is a line continuation character in Fortran, not a multiplication operator)
+
   """
-  Newcomb (https://apps.dtic.mil/sti/tr/pdf/ADA548492.pdf):
-  Long of Sun: 279° 41' 48''.04 + 129602768''.13 T
+  Newcomb (https://apps.dtic.mil/sti/tr/pdf/ADA548492.pdf Eqn 11):
+  Long of Sun: 279° 41' 48''.04 + 129602768''.13 T + 1.089''T^2
   where T is the time reckoned in Julian centuries of 36525 days since
   January 0, 1900, 12 h UT
 
-  279° 41' 48.04 = 279.6966777777777778 degrees
+  279° 41' 48.04 ≈ 279.6966777777777778 degrees
   129602768.13/36525/3600 ≈ 0.9856473353867213 degrees/day
 
   The difference in the leading constant in Russell 1971 and Newcomb's formula is
   279.6966777777777778 - 279.690983 = 0.005694777777762283 degrees
 
-  This seems to correspond to the aberration of Δλ = -20.4898'' = 0.00569161111
-  due to light-time correction for the Sun's apparent position (Meeus 1998, pg 133).
+  This seems to correspond to the aberration of Δλ = -20.4898'' = 0.00569161111 degrees
+  due to light-time correction for the Sun's apparent position (Meeus 1998, pg 133 "... affected
+  by aberration (-20''.5) and measured ...").
   """
 
   return gst_deg
 
 
-def newcomb(iyear, iday, secs):
-  fday = secs/86400.0
-  dj = 365*(iyear - 1900) + (iyear - 1901)//4 + iday + fday - 0.5
-  gst_deg = (279.6966777777777778 + 0.9856473354*dj + 180.0) % 360.0
-  return gst_deg
+def newcomb(iyear, iday, secs, order=2):
+  """Longitude of the Sun in degrees using Newcomb's formula."""
+  checkargs(iyear, iday, secs)
+
+  # Equation 11 of McCarthy 2011
+  #Long of Sun: 279° 41' 48''.04 + 129602768''.13 T + 1.089''T^2
+  #where T is the time reckoned in Julian centuries of 36525 days since
+  #January 0, 1900, 12 h UT
+
+  # 36525.0 = julian_days_since_j2000(2000, 1, 86400/2) - julian_days_since_j2000(1899, 365, 86400/2)# 
+  dj = julian_days_since_j2000(iyear, iday, secs) + 36525.0
+
+  T = dj/36525.0
+
+  a = 279 + 41/60.0 + 48.04/3600.0
+  b = (129602768.13)/3600.0
+  c = 1.08936525/3600.0
+  if order == 1:
+    c = 0.0
+
+  long = (a + b*T + c*T**2) % 360.0
+
+  # J1900.0 = 1899 December 31, 12:00 TT = JD 2415020.0
+  # J2000.0 = 2000 January 1, 12:00 TT = JD 2451545.0
+  # Difference is 1 Julian Century
+
+  # To convert to J2000.0, replace T with T + 1, which gives
+  # L = (a + b*(T+1) + c*(T+1)^2) % 360.0
+  # L = (a + b + c^2 + (b + 2*c)*T + c*T^2) % 360.0
+
+  # (a + b + c^2) % 360.0 = 280.46560286934255
+  # (b + 2*c) = 36000.76953020292
+  # long = (280.46560286934255 + 36000.76953020292*T + c*T**2) % 360.0
+
+  # Meeus 1998, Eqn 12.4 is
+  # T = (JD - 2451 545.0)/36525
+  #theta0 = 280.460 618 37 + 360.985 647 366 29 (JD - 2451 545.0) + 0.000 387 933*T^2 - T^3/38 710 000
+  return long
 
 
 def gmstF02(iyear, iday, secs):
@@ -224,12 +312,12 @@ def gmstF02(iyear, iday, secs):
   ------------------------------------------------------------------------------
 
   Note that 1/38710000 (= 2.5833118057349522e-08) is used in Meeus 1998 Eqn 12.4
-  (see notes in gmstM98) and 2.6e-08 is used in Fränz and Harper 2002 (and
+  (see notes in gmstM98) and 2.6e-08 is used in Fränz and Harper 2002 below (and
   Siedelman 1992). Also, 0.000387933 is used in Meeus 1998 Eqn 12.4
   (and Siedelman 1992) but 0.0003875 is used in Fränz and Harper 2002.
 
   Finally, Fränz and Harper 2002 cite "Meeus, J, 2000, Astronomical Algorithms
-  2nd Edition" but but I am only  able to find 1998 associated with a 2nd edition.
+  2nd Edition" but but I am only able to find 1998 associated with a 2nd edition.
   """
 
   checkargs(iyear, iday, secs)
@@ -238,11 +326,14 @@ def gmstF02(iyear, iday, secs):
   d0 = julian_days_since_j2000(iyear, iday, secs)
 
   # Fränz and Harper 2002, Eqn (20)
-  theta = 280.46061837 + 360.98564736629*d0 + 0.0003875*T0**2   - (2.6e-8)*T0**3
+  theta = 280.46061837 + 360.98564736629*d0 + 0.0003875*T0**2 - (2.6e-8)*T0**3
   """
   Meeus 1998, Eqn 12.4
   theta = 280.46061837 + 360.98564736629*d0 + 0.000387933*T0**2 - (1/38710000)*T0**3
   """
+  #print(0.0003875*T0**2)
+  #print(0.000387933*T0**2)
+
   theta = theta % 360.0
 
   return theta
@@ -330,7 +421,19 @@ def gmstH92(iyear, iday, secs):
   1 January 2000 (known as epoch 2000.0) to the previous midnight.
   ------------------------------------------------------------------------------
 
-  Note that equation (3) for theta in Hapgood 1992 is related to theta0 in 
+  Hapgood 1995, Section 3
+  ------------------------------------------------------------------------------
+  The Russell (1971) paper is more complex. GST and $\\lambda_{\\odot}$ are given
+  by the Fortran subroutine in his Appendix 2. The statements in that routine
+  are functionally equivalent to the equations in Hapgood (1992) and thus yield
+  values in the mean epoch-of-date.
+  ...
+
+  However, the values of GST and $\\lambda_{\\odot}$ predicted by Russell agree
+  with those of Hapgood (1992) to within $0.01^\\circ$.
+  ------------------------------------------------------------------------------
+
+  Note that equation (3) for theta in Hapgood (1992) is related to Theta0 in 
   Meeus 1998 (see notes in gmstM198). Meeus 1998 Eqn 12.3 is
 
   Theta0 = 100.46061837 + 36000.770053608*T + 0.000387933*T^2 - T^3/38710000.
@@ -357,46 +460,62 @@ def gmstH92(iyear, iday, secs):
 if __name__ == "__main__":
   #julian_days_since_j2000_test()
   #exit()
-  year = 2020
-  iday = 1
-  secs = 0.0
+  from utilrsw.xprint import xprint
 
   """
   Entering data into https://aa.usno.navy.mil/data/siderealtime gives
-  https://aa.usno.navy.mil/calculated/siderealtime?date=2000-01-01&time=00:00:00.000&intv_mag=1.00&intv_unit=1&reps=1&lat=0.0000&lon=0.0000&label=&submit=Get+Data
+
   Date (UT1)	Time (UT1)  GST (Mean)  GST (Apparent)  LST (Mean)    LST (Apparent) EoE
+
+  https://aa.usno.navy.mil/calculated/siderealtime?date=2000-01-01&time=00:00:00.000&intv_mag=1.00&intv_unit=1&reps=1&lat=0.0000&lon=0.0000&label=&submit=Get+Data
   2000-Jan-1	00:00:00.0	06:39:52.2717	06:39:51.4197	06:39:52.2717 06:39:51.4197  -0.8520
+
+  https://aa.usno.navy.mil/calculated/siderealtime?date=2000-01-01&time=12%3A00%3A00.000&intv_mag=1.00&intv_unit=1&reps=1&lat=0.0000&lon=0.0000&label=&submit=Get+Data
+  2000-Jan-1	12:00:00.0	18:41:50.5494	18:41:49.6974	18:41:50.5494	18:41:49.6974	-0.8520
+
   https://aa.usno.navy.mil/calculated/siderealtime?date=2020-01-01&time=00:00:00.000&intv_mag=1.00&intv_unit=1&reps=1&lat=0.0000&lon=0.0000&label=&submit=Get+Data
   2020-Jan-1	00:00:00.0	06:40:29.2343	06:40:28.2256	06:40:29.2343	06:40:28.2256	-1.0087
 
   where GST = Greenwich Sidereal Time, LST = Local Sidereal Time, and 
   EoE = Equation of the Equinoxes = GST (Apparent) - GST (Mean) = LST (Apparent) - LST (Mean)
   """
-  print(f"{year}-{iday:03d} {secs:.3f} sec")
-  if year == 2000 and iday == 1 and secs == 0.0:
-    gmstUSNO_str = "06:39:52.2717"
-  if year == 2020 and iday == 1 and secs == 0.0:
-    gmstUSNO_str = "06:40:29.2343"
 
-  gmstUSNO = timestr2deg(gmstUSNO_str)
-  gmstR71 = gmstR71(year, iday, secs)
-  gmstH92 = gmstH92(year, iday, secs)
-  gmstF02 = gmstF02(year, iday, secs)
-  gmstM98 = gmstM98(year, iday, secs)
+  tests = [
+    (2000, 1, 0, "06:39:52.2717"),
+    (2000, 1, 43200, "18:41:50.5494"),
+    (2020, 1, 0, "06:40:29.2343")
+  ]
 
-  print(gmstR71)
-  print(newcomb(year, iday, secs))
+  for year, iday, secs, usno_str in tests:
+    print(80*"-")
+    xprint(f"{year}-{iday:03d} {secs:.3f} sec")
 
-  #ref = "USNO"
-  #ref_val = timestr2deg(gmstUSNO_str)
-  ref = "R71"
-  ref_val = gmstR71
+    usno = timestr2deg(usno_str)
+    R71 = gmstR71(year, iday, secs)
+    H92 = gmstH92(year, iday, secs)
+    F02 = gmstF02(year, iday, secs)
+    M98 = gmstM98(year, iday, secs)
 
-  print(f"USNO         GMST = {gmstUSNO_str}   | {gmstUSNO:.6f} deg | {gmstUSNO - ref_val:9.6f} deg diff from {ref} value")
-  print(f"Russell 1971 GMST = {deg2timestr(gmstR71)} | {gmstR71:.6f} deg | {gmstR71 - ref_val:9.6f} deg diff from {ref} value")
-  print(f"Hapgood 1992 GMST = {deg2timestr(gmstH92)} | {gmstH92:.6f} deg | {gmstH92 - ref_val:9.6f} deg diff from {ref} value")
-  print(f"F & H 2002   GMST = {deg2timestr(gmstF02)} | {gmstF02:.6f} deg | {gmstF02 - ref_val:9.6f} deg diff from {ref} value")
-  print(f"Meeus 1998   GMST = {deg2timestr(gmstM98)} | {gmstM98:.6f} deg | {gmstM98 - ref_val:9.6f} deg diff from {ref} value")
+    xprint(f"Newcomb long of Sun (1st order only)  {newcomb(year, iday, secs, order=1):9.6f} deg")
+    xprint(f"Newcomb long of Sun (full equation)   {newcomb(year, iday, secs, order=2):9.6f} deg")
+    xprint("")
+
+    #ref = "USNO"
+    #ref_val = timestr2deg(usno_str)
+
+    ref = "R71"
+    ref_val = R71
+
+    xprint(f"USNO         GMST = {usno_str}   | {usno:.6f} deg | {usno - ref_val:9.6f} deg diff from {ref} value")
+    xprint(f"Russell 1971 GMST = {deg2timestr(R71)} | {R71:.6f} deg | {R71 - ref_val:9.6f} deg diff from {ref} value")
+    xprint(f"Hapgood 1992 GMST = {deg2timestr(H92)} | {H92:.6f} deg | {H92 - ref_val:9.6f} deg diff from {ref} value")
+    xprint(f"F & H 2002   GMST = {deg2timestr(F02)} | {F02:.6f} deg | {F02 - ref_val:9.6f} deg diff from {ref} value")
+    xprint(f"Meeus 1998   GMST = {deg2timestr(M98)} | {M98:.6f} deg | {M98 - ref_val:9.6f} deg diff from {ref} value")
+
+    xprint(f"\nF & H 2002 - Meeus 1998 {F02 - M98:.16e}")
+
+    print(80*"-")
+    print("")
 
 
   if False:
